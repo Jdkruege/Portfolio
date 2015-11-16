befb4979e0bad07d20dbc3abee08e9c76f3d3ab0
Jonathan Krueger <jdkruege@iastate.edu>
2015-11-16 00:52:03 -0600

    Reworked site design using bootstrap to create a cleaner feel. Ensured content was laid out better, and the site was easier on the eyes. I also condensed everything to one page and added animations for swapping between pages.

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	$('.navbar-nav li').on('click', function() {
+		changeActiveSection($(this));
+		slideContent($(this).attr('target'));	
+	});
+});
+
+function changeActiveSection(activeElement) {
+	$('.active-section').removeClass('active-section');
+	activeElement.addClass('active-section');
+}
+
+function slideContent(target) {
+	var incomingElement = $('#' + target);
+	var outgoingElement = $('.active-page');
+	if (incomingElement.attr('index') > outgoingElement.attr('index')) {
+		outgoingElement.hide('slide', {direction: 'left'}, 200, function() {
+			outgoingElement.removeClass('active-page');
+		});
+		incomingElement.show('slide', {direction: 'right'}, 200, function() {
+			incomingElement.addClass('active-page');
+		});
+	} else if (incomingElement.attr('index') < outgoingElement.attr('index')) {
+		outgoingElement.hide('slide', {direction: 'right'}, 200, function() {
+			outgoingElement.removeClass('active-page');
+		});
+		incomingElement.show('slide', {direction: 'left'}, 200, function() {
+			incomingElement.addClass('active-page');
+		});
+	}
+}
